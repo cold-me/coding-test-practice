7104f49e63ddce74f96681bbc59d1361984c618f
cold-me <radiantbeautykim@gmail.com>
2025-02-18 23:29:09 +0900

    [level 1] Title: 유연근무제, Time: 2.31 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,24 +1,15 @@
 const timeToMinutes = (time) => Math.floor(time / 100) * 60 + (time % 100);
 function solution(schedules, timelogs, startday) {
     let count = 0;
-    for (let i = 0; i < schedules.length; i++) {
-        const startTime = timeToMinutes(schedules[i]);
+    return schedules.filter((schedule, i) => {
+        const startTime = timeToMinutes(schedule);
         const endTime = startTime + 10;
         let today = startday;
-        let flag = false;
-        for (let j = 0; j < timelogs[i].length; j++) {
-            if (today % 7 === 6 || today % 7 === 0) {
+        return timelogs[i].every((timelog) => {
+            if (today % 7 === 6 || today % 7 === 0 || timeToMinutes(timelog) <= endTime) {
                 today++;
-                continue;
-            }
-            const day = timeToMinutes(timelogs[i][j]);
-            if (endTime < day) {
-                flag = true;
-                break;
-            }
-            today++;
-        }
-        if (!flag) count++;
-    }
-    return count;
+                return true;
+            }else return false
+        });
+    }).length;
 }
