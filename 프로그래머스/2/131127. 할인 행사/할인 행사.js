20f1309dc1354918a3e1f95ac1e2b085f5527df1
cold-me <radiantbeautykim@gmail.com>
2025-03-12 17:00:00 +0900

    [level 2] Title: 할인 행사, Time: 47.32 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,20 @@
+function solution(want, number, discount) {
+    let TOTALNUMBER = number.reduce((acc, el)=> acc + el, 0);
+    let answer = 0;
+    for(let d = 0 ; d < discount.length; d++){
+        const goods = new Map();
+        want.forEach((item, i)=>{
+            goods.set(item, number[i]);
+        });
+        let flag = true;
+        for(let n = d; n < d + TOTALNUMBER ; n++){
+            if(!goods.has(discount[n]) || goods.get(discount[n]) <= 0){
+                flag = false;
+                break;
+            }
+            goods.set(discount[n], goods.get(discount[n]) - 1);
+        }
+        if(flag) answer++;
+    }
+    return answer;
+}
