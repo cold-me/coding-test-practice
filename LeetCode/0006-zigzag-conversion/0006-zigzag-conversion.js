a630a4fab3d55b509cda101a3b28ea19c3f4eee1
cold-me <radiantbeautykim@gmail.com>
2025-03-18 16:53:18 +0900

    Time: 155 ms (8.15%), Space: 83.1 MB (5%) - LeetHub

--- LeetCode/0006-zigzag-conversion/0006-zigzag-conversion.js
+++ LeetCode/0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -4,22 +4,24 @@
  * @return {string}
  */
 const convert = function (s, numRows) {
+    if (numRows === 1) return s
+
     let i = 0, j = 0, times = 0, asc = true;
     const map = new Map();
     map.set([i++, j], s[times++]);
-    if(numRows === 1) return s
+
     while (times < s.length) {
         if (i === 0 || i === numRows - 1) asc = !asc;
         map.set([i, j], s[times++])
-        if (asc) i++;
-        else (i--, j++);
+        asc ? i++ : i--, j++;
     }
+
     const answers = Array.from({ length: numRows }, () => Array(j).fill(0))
-    for(const [k, v] of map){
+    for (const [k, v] of map) {
         const [row, col] = k;
         answers[row][col] = v;
     }
-    return answers.map((item)=>{
-        return item.filter((i)=> i).join('')
+    return answers.map((item) => {
+        return item.filter((i) => i).join('')
     }).join('')
 };
